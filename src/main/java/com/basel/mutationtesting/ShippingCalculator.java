d002798c795c4419c585838b4b36d7a8fee7db7c
basel.obied <basel.obied@fawry.com>
2026-07-04 20:28:03 +0300

    mutate: branch 1 to use > instead

--- src/main/java/com/basel/mutationtesting/ShippingCalculator.java
+++ src/main/java/com/basel/mutationtesting/ShippingCalculator.java
@@ -8,7 +8,7 @@ public class ShippingCalculator {
     public static final double ZERO_SHIPPING_COST = 0.0;
 
     public double calculateShipping(double orderTotal, double weightKg, boolean isPremiumMember) {
-        if (isPremiumMember || orderTotal >= FREE_SHIPPING_THRESHOLD) {
+        if (isPremiumMember || orderTotal > FREE_SHIPPING_THRESHOLD) {
             return ZERO_SHIPPING_COST;
         }
 
