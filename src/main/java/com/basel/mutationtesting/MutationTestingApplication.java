eaf85055dce7cdf97df263ba411ad43d1bc5ce5c
basel.obied <basel.obied@fawry.com>
2026-07-04 19:44:26 +0300

    init

---
+++ src/main/java/com/basel/mutationtesting/MutationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.basel.mutationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutationTestingApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MutationTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basel/mutationtesting/MutationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basel.mutationtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutationTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
